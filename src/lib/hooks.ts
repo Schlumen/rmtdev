1e894f268cd8198990582cdc23c6feb2281bf71c
Schlumen <lumen.haendler@web.de>
2024-06-24 09:36:36 +0200

    Display individual JobItem

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,5 +1,42 @@
 import { useState, useEffect } from "react";
-import { JobItem } from "./types";
+import { JobItem, JobItemExtended } from "./types";
+import { BASE_API_URL } from "./constants";
+
+export function useActiveId() {
+  const [activeId, setActiveId] = useState<number | null>(null);
+
+  useEffect(() => {
+    const handleHashChange = () => {
+      const id = +window.location.hash.slice(1);
+      setActiveId(id);
+    };
+    handleHashChange();
+
+    window.addEventListener("hashchange", handleHashChange);
+
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange);
+    };
+  }, []);
+
+  return activeId;
+}
+
+export function useJobItem(id: number | null) {
+  const [jobItem, setJobItem] = useState<JobItemExtended | null>(null);
+
+  useEffect(() => {
+    if (!id) return;
+
+    (async () => {
+      const response = await fetch(`${BASE_API_URL}/${id}`);
+      const data = await response.json();
+      setJobItem(data.jobItem);
+    })();
+  }, [id]);
+
+  return jobItem;
+}
 
 export function useJobItems(searchText: string) {
   const [jobItems, setJobItems] = useState<JobItem[]>([]);
@@ -12,9 +49,7 @@ export function useJobItems(searchText: string) {
 
     (async () => {
       setIsLoading(true);
-      const response = await fetch(
-        `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
-      );
+      const response = await fetch(`${BASE_API_URL}?search=${searchText}`);
       const data = await response.json();
       setIsLoading(false);
       setJobItems(data.jobItems);
