ef0806be406256f98b38639ec2d982a37e713bf3
Schlumen <lumen.haendler@web.de>
2024-06-29 12:10:43 +0200

    Loading Job Spinner

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -24,18 +24,21 @@ export function useActiveId() {
 
 export function useJobItem(id: number | null) {
   const [jobItem, setJobItem] = useState<JobItemExtended | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (!id) return;
 
     (async () => {
+      setIsLoading(true);
       const response = await fetch(`${BASE_API_URL}/${id}`);
       const data = await response.json();
+      setIsLoading(false);
       setJobItem(data.jobItem);
     })();
   }, [id]);
 
-  return jobItem;
+  return [jobItem, isLoading] as const;
 }
 
 export function useJobItems(searchText: string) {
