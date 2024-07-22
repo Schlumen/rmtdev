8523538c5a13d8c20da98d7a5ad4c2d86e970ef9
Schlumen <lumen.haendler@web.de>
2024-07-22 16:40:20 +0200

    custom hook for click outside

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -161,6 +161,25 @@ export function useBookmarksContext() {
   return context;
 }
 
+export function useOnClickOutside(
+  refs: React.RefObject<HTMLElement>[],
+  handler: () => void
+) {
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (refs.every(ref => !ref.current?.contains(e.target as Node))) {
+        handler();
+      }
+    };
+
+    document.addEventListener("click", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, [refs, handler]);
+}
+
 /*
 Old functions without react query for reference:
 
